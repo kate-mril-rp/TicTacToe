12d91def5a1c47443070bc4ca2d8cb3dbbe75b9b
kate <Hawaii2013!>
2015-02-13 12:29:02 -0800

    Initial Commit

---
+++ ticTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ticTacToe
+//
+//  Created by Kate on 2/13/15.
+//  Copyright (c) 2015 game. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
